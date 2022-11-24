33ed0abbc5872137ff270e76f938ffd15d599360
maritzee <amby.121192@gmail.com>
2022-11-24 00:29:33 -0600

    👩‍🏫 Add presentation: Binomial distribution and code

---
+++ src/binomial_distribution.py
@@ -0,0 +1,25 @@
+from scipy.stats import binom
+import matplotlib.pyplot as plt
+
+n=20
+p=0.8
+k_values = list(range(n+1))
+
+mean, var = binom.stats(n, p)
+print("mean: ",mean, "var: ",var)
+#mean: número de tiros exitosos en promedio
+#var: 
+
+distribution = [binom.pmf(k, n, p) for k in k_values]
+#Probability Mass Function (PMF): Probabilidad total de lograr r éxito y nr fracasos
+
+
+plt.plot(k_values,distribution, color="black", linestyle="dashed")
+plt.bar(k_values,distribution, color="gray")
+plt.title("Distribución Binomial")
+plt.ylabel("Probabilidad(k)")
+plt.xlabel("Valores k (lanzamientos exitosos)")
+filepath = "slides/figures/binomial_distribution_normal.png"
+plt.savefig(filepath)
+plt.clf()
+
