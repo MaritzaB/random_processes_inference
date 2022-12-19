41f093e0be64f0d964e48ae9d43b28f3287b61b8
maritzee <amby.121192@gmail.com>
2022-12-18 18:20:41 -0600

    💫📝 Update report. Random walk.

---
+++ src/random_walk.py
@@ -0,0 +1,31 @@
+import random
+import math
+import matplotlib.pyplot as plt
+
+
+steps = 100
+step_weigth = 1
+
+x=y=0
+x_list = [x]
+y_list = [y]
+
+listax=[x]  
+listay=[y]  
+for i in range(steps+1):
+  angulo=random.random()*math.pi*2
+  x+=step_weigth*math.cos(angulo)
+  y+=step_weigth*math.sin(angulo)
+  listax.append(x)
+  listay.append(y)
+
+
+
+# Plot random_walk
+plt.plot(listax,listay, color="black")
+plt.axis('equal')
+plt.title("Caminata aleatoria")
+filepath = "reports/figures/Random_walk.png"
+plt.savefig(filepath)
+plt.clf()
+
